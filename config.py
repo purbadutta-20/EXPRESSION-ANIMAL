cb6c20e7f0e8a332bf5dfa71fe8dd830d0aa1510
purbadutta-20 <purbadutta2005@gmail.com>
2026-02-18 00:44:21 +0530

    Removed venv from repository

--- config.py
+++
@@ -1,9 +0,0 @@
-CAMERA_INDEX = 0
-HOLD_TIME = 0.6
-
-IMAGE_PATHS = {
-    "happy": "animals/smile.jpeg",
-    "surprise": "animals/surprise.jpeg",
-    "thumbs_up": "animals/thumbsup.jpeg",
-    "wink": "animals/wink.jpeg"
-}
